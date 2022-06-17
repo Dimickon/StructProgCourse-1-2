8177a5cfed38764716b45748a576466389f0ad58
Dimickon <dimickon1@gmail.com>
2022-06-17 19:40:41 +0300

    add title

--- Project1/Project1/MyForm.h
+++ Project1/Project1/MyForm.h
@@ -34,6 +34,9 @@ namespace Project1 {
 				delete components;
 			}
 		}
+	private: System::ComponentModel::BackgroundWorker^ backgroundWorker1;
+	protected:
+	private: System::Windows::Forms::Label^ label1;
 
 	private:
 		/// <summary>
@@ -48,12 +51,36 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->backgroundWorker1 = (gcnew System::ComponentModel::BackgroundWorker());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(366, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(303, 31);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Треугольник Серпинского";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1046, 541);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"Треугольник Серпинского";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
 	};
 }
