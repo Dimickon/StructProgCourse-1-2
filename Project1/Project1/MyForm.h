89892ba6f99b5bf05ac9f279d33fc84a24de14d0
Dimickon <dimickon1@gmail.com>
2022-06-18 16:39:03 +0300

    beta 1.1

--- Project1/Project1/MyForm.h
+++ Project1/Project1/MyForm.h
@@ -37,6 +37,9 @@ namespace Project1 {
 	private: System::ComponentModel::BackgroundWorker^ backgroundWorker1;
 	protected:
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::ColorDialog^ colorDialog1;
 
 	private:
 		/// <summary>
@@ -53,6 +56,10 @@ namespace Project1 {
 		{
 			this->backgroundWorker1 = (gcnew System::ComponentModel::BackgroundWorker());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->colorDialog1 = (gcnew System::Windows::Forms::ColorDialog());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -62,19 +69,41 @@ namespace Project1 {
 				static_cast<System::Byte>(204)));
 			this->label1->Location = System::Drawing::Point(366, 9);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(303, 31);
+			this->label1->Size = System::Drawing::Size(349, 36);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Треугольник Серпинского";
 			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(37, 76);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(678, 410);
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 20.29091F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(779, 403);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(225, 83);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Сохранить";
+			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1046, 541);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->label1);
 			this->Name = L"MyForm";
 			this->Text = L"Треугольник Серпинского";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
