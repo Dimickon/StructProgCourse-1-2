3724936b5c4ad2ed379356cf44a796a719921f44
Dimickon <dimickon1@gmail.com>
2022-06-19 18:07:26 +0300

    zatup

--- Project1/Project1/MyForm.h
+++ Project1/Project1/MyForm.h
@@ -34,12 +34,24 @@ namespace Project1 {
 				delete components;
 			}
 		}
-	private: System::ComponentModel::BackgroundWorker^ backgroundWorker1;
+
 	protected:
 	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::PictureBox^ okno;
+
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::ColorDialog^ colorDialog1;
+
+
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::NumericUpDown^ numericUpDown1;
+	private: System::Windows::Forms::NumericUpDown^ numericUpDown2;
+	private: System::Windows::Forms::Button^ btncreate;
+	private: System::Windows::Forms::TextBox^ answer;
+
+
+
+
 
 	private:
 		/// <summary>
@@ -54,12 +66,18 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->backgroundWorker1 = (gcnew System::ComponentModel::BackgroundWorker());
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->okno = (gcnew System::Windows::Forms::PictureBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->colorDialog1 = (gcnew System::Windows::Forms::ColorDialog());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->numericUpDown2 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->btncreate = (gcnew System::Windows::Forms::Button());
+			this->answer = (gcnew System::Windows::Forms::TextBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->okno))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -67,49 +85,129 @@ namespace Project1 {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(366, 9);
+			this->label1->Location = System::Drawing::Point(377, 9);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(349, 36);
+			this->label1->RightToLeft = System::Windows::Forms::RightToLeft::No;
+			this->label1->Size = System::Drawing::Size(303, 31);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Треугольник Серпинского";
-			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			this->label1->TextAlign = System::Drawing::ContentAlignment::TopCenter;
 			// 
-			// pictureBox1
+			// okno
 			// 
-			this->pictureBox1->Location = System::Drawing::Point(37, 76);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(678, 410);
-			this->pictureBox1->TabIndex = 1;
-			this->pictureBox1->TabStop = false;
+			this->okno->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->okno->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->okno->Location = System::Drawing::Point(40, 76);
+			this->okno->Name = L"okno";
+			this->okno->Size = System::Drawing::Size(678, 410);
+			this->okno->TabIndex = 1;
+			this->okno->TabStop = false;
 			// 
 			// button1
 			// 
+			this->button1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Right));
 			this->button1->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 20.29091F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button1->Location = System::Drawing::Point(779, 403);
+			this->button1->Location = System::Drawing::Point(777, 439);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(225, 83);
+			this->button1->Size = System::Drawing::Size(225, 47);
 			this->button1->TabIndex = 2;
 			this->button1->Text = L"Сохранить";
 			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this->label2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(775, 76);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(142, 20);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Высота треугольника";
+			// 
+			// label3
+			// 
+			this->label3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(775, 137);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(148, 20);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Сторона треугольника";
+			// 
+			// numericUpDown1
+			// 
+			this->numericUpDown1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->numericUpDown1->Location = System::Drawing::Point(779, 99);
+			this->numericUpDown1->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 10, 0, 0, 0 });
+			this->numericUpDown1->Name = L"numericUpDown1";
+			this->numericUpDown1->Size = System::Drawing::Size(223, 20);
+			this->numericUpDown1->TabIndex = 7;
+			// 
+			// numericUpDown2
+			// 
+			this->numericUpDown2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->numericUpDown2->Location = System::Drawing::Point(779, 160);
+			this->numericUpDown2->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 10000, 0, 0, 0 });
+			this->numericUpDown2->Name = L"numericUpDown2";
+			this->numericUpDown2->Size = System::Drawing::Size(223, 20);
+			this->numericUpDown2->TabIndex = 8;
+			// 
+			// btncreate
+			// 
+			this->btncreate->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Right));
+			this->btncreate->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 20.29091F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->btncreate->Location = System::Drawing::Point(777, 373);
+			this->btncreate->Name = L"btncreate";
+			this->btncreate->Size = System::Drawing::Size(225, 48);
+			this->btncreate->TabIndex = 9;
+			this->btncreate->Text = L"Построить";
+			this->btncreate->UseVisualStyleBackColor = true;
+			this->btncreate->Click += gcnew System::EventHandler(this, &MyForm::btncreate_Click);
+			// 
+			// answer
+			// 
+			this->answer->Location = System::Drawing::Point(779, 244);
+			this->answer->Name = L"answer";
+			this->answer->Size = System::Drawing::Size(100, 20);
+			this->answer->TabIndex = 10;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1046, 541);
+			this->Controls->Add(this->answer);
+			this->Controls->Add(this->btncreate);
+			this->Controls->Add(this->numericUpDown2);
+			this->Controls->Add(this->numericUpDown1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->button1);
-			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->okno);
 			this->Controls->Add(this->label1);
+			this->MaximumSize = System::Drawing::Size(1280, 720);
 			this->Name = L"MyForm";
 			this->Text = L"Треугольник Серпинского";
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->okno))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+		
+	
+	private: System::Void btncreate_Click(System::Object^ sender, System::EventArgs^ e) {
+		float center = (okno->Width) / 2;
 	}
-	};
+};
 }
