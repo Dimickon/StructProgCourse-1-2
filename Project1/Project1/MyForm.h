b64020d33592a76ed4af766a6e6ea896f00df25d
Dimickon <dimickon1@gmail.com>
2022-06-24 22:45:17 +0300

    Finish
    Нужно только доделать обновление цвета фона

--- Project1/Project1/MyForm.h
+++ Project1/Project1/MyForm.h
@@ -61,6 +61,7 @@ namespace Project1 {
 
 	private: System::Windows::Forms::Button^ btncreate;
 	private: System::Windows::Forms::SaveFileDialog^ saveFileDialog1;
+	private: System::Windows::Forms::Label^ label4;
 
 
 
@@ -87,6 +88,7 @@ namespace Project1 {
 			this->levelTriagle = (gcnew System::Windows::Forms::NumericUpDown());
 			this->btncreate = (gcnew System::Windows::Forms::Button());
 			this->saveFileDialog1 = (gcnew System::Windows::Forms::SaveFileDialog());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->colorPick = (gcnew System::Windows::Forms::Button());
 			this->colorDialog1 = (gcnew System::Windows::Forms::ColorDialog());
 			this->colorPickBG = (gcnew System::Windows::Forms::Button());
@@ -97,13 +99,19 @@ namespace Project1 {
 			// 
 			// label1
 			// 
+			this->label1->Anchor = System::Windows::Forms::AnchorStyles::Top;
 			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(36)), static_cast<System::Int32>(static_cast<System::Byte>(36)),
+				static_cast<System::Int32>(static_cast<System::Byte>(36)));
 			this->label1->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(377, 9);
+			this->label1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(204)), static_cast<System::Int32>(static_cast<System::Byte>(204)),
+				static_cast<System::Int32>(static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(392, 9);
 			this->label1->Name = L"label1";
+			this->label1->Padding = System::Windows::Forms::Padding(5);
 			this->label1->RightToLeft = System::Windows::Forms::RightToLeft::No;
-			this->label1->Size = System::Drawing::Size(303, 31);
+			this->label1->Size = System::Drawing::Size(313, 41);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Треугольник Серпинского";
 			this->label1->TextAlign = System::Drawing::ContentAlignment::TopCenter;
@@ -113,8 +121,10 @@ namespace Project1 {
 			this->okno->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
 				| System::Windows::Forms::AnchorStyles::Left)
 				| System::Windows::Forms::AnchorStyles::Right));
+			this->okno->BackColor = System::Drawing::Color::Lavender;
 			this->okno->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
 			this->okno->Location = System::Drawing::Point(40, 76);
+			this->okno->MinimumSize = System::Drawing::Size(300, 200);
 			this->okno->Name = L"okno";
 			this->okno->Padding = System::Windows::Forms::Padding(10);
 			this->okno->Size = System::Drawing::Size(678, 410);
@@ -124,25 +134,33 @@ namespace Project1 {
 			// saveTriagle
 			// 
 			this->saveTriagle->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Right));
-			this->saveTriagle->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 20.29091F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->saveTriagle->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(36)), static_cast<System::Int32>(static_cast<System::Byte>(36)),
+				static_cast<System::Int32>(static_cast<System::Byte>(36)));
+			this->saveTriagle->Font = (gcnew System::Drawing::Font(L"Arial", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
+			this->saveTriagle->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(204)), static_cast<System::Int32>(static_cast<System::Byte>(204)),
+				static_cast<System::Int32>(static_cast<System::Byte>(204)));
 			this->saveTriagle->Location = System::Drawing::Point(779, 439);
 			this->saveTriagle->Name = L"saveTriagle";
 			this->saveTriagle->Size = System::Drawing::Size(225, 47);
 			this->saveTriagle->TabIndex = 2;
 			this->saveTriagle->Text = L"Сохранить";
-			this->saveTriagle->UseVisualStyleBackColor = true;
+			this->saveTriagle->UseVisualStyleBackColor = false;
 			this->saveTriagle->Click += gcnew System::EventHandler(this, &MyForm::saveTriagle_Click);
 			// 
 			// label2
 			// 
 			this->label2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
 			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(36)), static_cast<System::Int32>(static_cast<System::Byte>(36)),
+				static_cast<System::Int32>(static_cast<System::Byte>(36)));
+			this->label2->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(775, 76);
+			this->label2->ForeColor = System::Drawing::Color::White;
+			this->label2->Location = System::Drawing::Point(776, 76);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(142, 20);
+			this->label2->Padding = System::Windows::Forms::Padding(20, 12, 20, 12);
+			this->label2->Size = System::Drawing::Size(228, 49);
 			this->label2->TabIndex = 4;
 			this->label2->Text = L"Высота треугольника";
 			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
@@ -150,58 +168,89 @@ namespace Project1 {
 			// levelTriagle
 			// 
 			this->levelTriagle->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
-			this->levelTriagle->Location = System::Drawing::Point(779, 99);
+			this->levelTriagle->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(16)), static_cast<System::Int32>(static_cast<System::Byte>(16)),
+				static_cast<System::Int32>(static_cast<System::Byte>(16)));
+			this->levelTriagle->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(198)), static_cast<System::Int32>(static_cast<System::Byte>(198)),
+				static_cast<System::Int32>(static_cast<System::Byte>(198)));
+			this->levelTriagle->Location = System::Drawing::Point(775, 128);
 			this->levelTriagle->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 10, 0, 0, 0 });
 			this->levelTriagle->Name = L"levelTriagle";
-			this->levelTriagle->Size = System::Drawing::Size(223, 20);
+			this->levelTriagle->Size = System::Drawing::Size(229, 20);
 			this->levelTriagle->TabIndex = 7;
+			this->levelTriagle->ValueChanged += gcnew System::EventHandler(this, &MyForm::levelTriagle_ValueChanged);
 			// 
 			// btncreate
 			// 
 			this->btncreate->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Right));
-			this->btncreate->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 20.29091F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->btncreate->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(36)), static_cast<System::Int32>(static_cast<System::Byte>(36)),
+				static_cast<System::Int32>(static_cast<System::Byte>(36)));
+			this->btncreate->Font = (gcnew System::Drawing::Font(L"Arial", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
+			this->btncreate->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(204)), static_cast<System::Int32>(static_cast<System::Byte>(204)),
+				static_cast<System::Int32>(static_cast<System::Byte>(204)));
 			this->btncreate->Location = System::Drawing::Point(779, 385);
 			this->btncreate->Name = L"btncreate";
 			this->btncreate->Size = System::Drawing::Size(225, 48);
 			this->btncreate->TabIndex = 9;
 			this->btncreate->Text = L"Построить";
-			this->btncreate->UseVisualStyleBackColor = true;
+			this->btncreate->UseVisualStyleBackColor = false;
 			this->btncreate->Click += gcnew System::EventHandler(this, &MyForm::btncreate_Click);
+			// 
+			// label4
+			// 
+			this->label4->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(775, 196);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(0, 20);
+			this->label4->TabIndex = 10;
 			// 
 			// colorPick
 			// 
 			this->colorPick->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Right));
-			this->colorPick->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 20.29091F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->colorPick->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(36)), static_cast<System::Int32>(static_cast<System::Byte>(36)),
+				static_cast<System::Int32>(static_cast<System::Byte>(36)));
+			this->colorPick->Font = (gcnew System::Drawing::Font(L"Arial", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
+			this->colorPick->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(204)), static_cast<System::Int32>(static_cast<System::Byte>(204)),
+				static_cast<System::Int32>(static_cast<System::Byte>(204)));
 			this->colorPick->Location = System::Drawing::Point(779, 331);
 			this->colorPick->Name = L"colorPick";
 			this->colorPick->Size = System::Drawing::Size(225, 48);
 			this->colorPick->TabIndex = 12;
 			this->colorPick->Text = L"Цвет треугольника";
-			this->colorPick->UseVisualStyleBackColor = true;
+			this->colorPick->UseVisualStyleBackColor = false;
 			this->colorPick->Click += gcnew System::EventHandler(this, &MyForm::colorPick_Click);
 			// 
 			// colorPickBG
 			// 
 			this->colorPickBG->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Right));
-			this->colorPickBG->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 20.29091F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->colorPickBG->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(36)), static_cast<System::Int32>(static_cast<System::Byte>(36)),
+				static_cast<System::Int32>(static_cast<System::Byte>(36)));
+			this->colorPickBG->Font = (gcnew System::Drawing::Font(L"Arial", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
+			this->colorPickBG->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(204)), static_cast<System::Int32>(static_cast<System::Byte>(204)),
+				static_cast<System::Int32>(static_cast<System::Byte>(204)));
 			this->colorPickBG->Location = System::Drawing::Point(779, 277);
 			this->colorPickBG->Name = L"colorPickBG";
 			this->colorPickBG->Size = System::Drawing::Size(225, 48);
 			this->colorPickBG->TabIndex = 13;
 			this->colorPickBG->Text = L"Цвет фона";
-			this->colorPickBG->UseVisualStyleBackColor = true;
+			this->colorPickBG->UseVisualStyleBackColor = false;
 			this->colorPickBG->Click += gcnew System::EventHandler(this, &MyForm::colorPickBG_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(18)), static_cast<System::Int32>(static_cast<System::Byte>(18)),
+				static_cast<System::Int32>(static_cast<System::Byte>(18)));
 			this->ClientSize = System::Drawing::Size(1046, 541);
 			this->Controls->Add(this->colorPickBG);
 			this->Controls->Add(this->colorPick);
+			this->Controls->Add(this->label4);
 			this->Controls->Add(this->btncreate);
 			this->Controls->Add(this->levelTriagle);
 			this->Controls->Add(this->label2);
@@ -211,6 +260,7 @@ namespace Project1 {
 			this->MaximumSize = System::Drawing::Size(1280, 720);
 			this->Name = L"MyForm";
 			this->Text = L"Треугольник Серпинского";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->okno))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->levelTriagle))->EndInit();
 			this->ResumeLayout(false);
@@ -307,5 +357,9 @@ namespace Project1 {
 			okno->BackColor = colorDialog2->Color;
 		}
 	}
+private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void levelTriagle_ValueChanged(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
